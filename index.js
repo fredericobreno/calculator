8167fbfd537922997baea8f52dcc3b2c19c83d2d
Frederico Breno <frederico.breno@beon.studio>
2022-08-16 11:49:34 -0300

    fixing js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 let value1 = "";
 let currentOperator = "";
+let prevCurrentOperator = "";
 let value2 = "";
+let prevValue2 = "";
 let display = "";
 let reset = false;
 const keyEls = document.querySelectorAll("#keys > div");
 const inputEl = document.querySelector("#calculator > input");
-
-const updateDisplay = () => {
-  inputEl.value = display;
-  console.log(`v1: ${value1}\nop: ${currentOperator}\nv2: ${value2}`);
-};
 
 const onKeyClick = (e) => {
   const value = e.target.innerText;
@@ -25,15 +22,30 @@ const onKeyClick = (e) => {
     value1 = sqrt;
     currentOperator = "";
     value2 = "";
+    reset = true;
   } else if (value === "=") {
-    let result = eval(`${value1}${currentOperator}${value2}`);
-    display = result;
-    value1 = result;
+    if (value1 && currentOperator && value2) {
+      let result = eval(`${value1}${currentOperator}${value2}`);
+      prevValue2 = value2;
+      prevCurrentOperator = currentOperator;
+      display = result;
+      value1 = result;
+    } else if (!currentOperator) {
+      currentOperator = prevCurrentOperator;
+      value2 = prevValue2;
+      let result = eval(`${value1}${currentOperator}${value2}`);
+      display = result;
+      value1 = result;
+    }
     currentOperator = "";
     value2 = "";
     reset = true;
   } else if (!isNaN(value)) {
-    if (reset) display = "";
+    if (reset) {
+      value1 = "";
+      display = "";
+      reset = false;
+    }
     currentOperator ? (value2 += value) : (value1 += value);
     display += value;
   } else {
@@ -49,7 +61,7 @@ const onKeyClick = (e) => {
     currentOperator = value;
   }
 
-  updateDisplay();
+  inputEl.value = display;
 };
 
 keyEls.forEach((keyEl) => {
